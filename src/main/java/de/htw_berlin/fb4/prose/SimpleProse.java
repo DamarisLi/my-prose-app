6173eb8f9207ca22b33a7bfa96f653eaff8159dd
Damaris <damaris.liedtke@student.htw-berlin.de>
2025-06-29 18:09:41 +0200

    Add SimpleProse functionality

---
+++ src/main/java/de/htw_berlin/fb4/prose/SimpleProse.java
@@ -0,0 +1,23 @@
+package de.htw_berlin.fb4.prose;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import de.htw_berlin.fb4.ossd.prose.Prose;
+import de.htw_berlin.fb4.ossd.prose.Sentence;
+
+public class SimpleProse implements Prose {
+    private final List<Sentence> sentences;
+
+    public SimpleProse(List<Sentence> sentences) {
+        this.sentences = sentences;
+    }
+
+    @Override
+    public String get() {
+        return sentences.stream()
+                .map(Sentence::get)
+                .collect(Collectors.joining(" "));
+    }
+    
+}
