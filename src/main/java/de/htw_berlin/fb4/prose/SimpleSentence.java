444b9db17f65e353bd07e6c2f5cd2c16e2a9e271
Damaris <damaris.liedtke@student.htw-berlin.de>
2025-06-30 01:45:50 +0200

    Add license headers

--- src/main/java/de/htw_berlin/fb4/prose/SimpleSentence.java
+++ src/main/java/de/htw_berlin/fb4/prose/SimpleSentence.java
@@ -1,3 +1,12 @@
+/*
+ * ProseApp - A simple prose builder application
+ * Copyright (C) 2025 Damaris Liedtke
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * which is available at https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html.
+ */
+
 package de.htw_berlin.fb4.prose;
 
 import de.htw_berlin.fb4.ossd.prose.Sentence;
